cad317857f66adb480f9e99543a5f200ac66db58
ys-6-1 <72680138+ys-6-1@users.noreply.github.com>
2022-07-12 12:01:57 +0900

    fix: fix total_time sorting issue when new category has duration of null

--- server/models/category.js
+++ server/models/category.js
@@ -1,12 +1,21 @@
 const knex = require("../knex");
 const CATEGORY_TABLE = "category";
+const ACTION_TABLE = "action";
 
 module.exports = {
   getById(id) {
     return knex.select().from(CATEGORY_TABLE).where({ id }).first();
   },
-  create(category) {
-    return knex(CATEGORY_TABLE).insert(category);
+  async create(category) {
+    const response = await knex(CATEGORY_TABLE).insert(category).returning("*");
+
+    const action = {
+      duration: 0,
+      category_id: response[0].id,
+      user_id: response[0].user_id,
+      date: new Date(),
+    };
+    return knex(ACTION_TABLE).insert(action);
   },
   update(update, id) {
     update.updated_at = new Date();
